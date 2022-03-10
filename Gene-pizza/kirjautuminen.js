bc25f582afb48b8ff89e789fd913ecc958bede1d
MiloVilen <99165692+MiloVilen@users.noreply.github.com>
2022-03-10 18:33:07 +0200

    kyllä

--- Gene-pizza/kirjautuminen.js
+++ Gene-pizza/kirjautuminen.js
@@ -1,8 +1,18 @@
-
-
-function kirjaudu(){
-    localStorage.setItem("nimi",document.getElementById("nimi").value);
+document.addEventListener("DOMContentLoaded", aloitus);
+function aloitus(){
+    if (localStorage.getItem("kirjautunut")==="kylla"){
+        document.getElementById("kirjaudu_ulos");
+    } 
+    else {
+        document.getElementById("kirjaudu_ulos"). style.display ="none";
+    }
 }
-function kirjaudu(){
-    localStorage.setItem("Salasana",document.getElementById("Salasana").value);
+
+function registeroidy(){
+    localStorage.setItem("Nimi",document.getElementById("nimi").value);
+    localStorage.setItem("salasana",document.getElementById("salasana").value);
+    localStorage.setItem("kirjautunut", "kylla");
+}
+function uloskirjaus(){
+    localStorage.clear();
 }
